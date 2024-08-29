d8e4c18f769f7ab43e1461c80d9c66c6ad5c562e
larteyhakim <65889551+larteyhakim@users.noreply.github.com>
2024-08-29 18:04:42 +0000

    error when exporting items list to PDF

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -9,24 +9,24 @@ use Illuminate\Support\Facades\Auth;
 
 class PDFController extends Controller
 {
-    public function downloadItemsListPdf(){
+    public function downloadItemsListPdf()
+    {
+        ini_set('memory_limit', '512M'); // Increase memory limit
+        
         $site_id = Auth::user()->site->id;
-        $items = InventoryItem::join('items', 'inventory_items.item_id', '=', 'items.id')
+        $pdf = PDF::loadView('pdfs.items_list_site_pdf', ['items' => []]); // Initialize PDF
+    
+        InventoryItem::join('items', 'inventory_items.item_id', '=', 'items.id')
             ->join('locations', 'locations.id', '=', 'inventory_items.location_id')
             ->join('inventories', 'inventories.id', '=', 'inventory_items.inventory_id')
             ->where('inventory_items.site_id', '=', $site_id)
-            ->where('inventory_items.quantity', '>', '0')->get();
-
-        // $pdf = PDF::loadView('pdfs.items_list_site_pdf', compact('items'));
-        // return $pdf->download('items_list.pdf');
-        $inventory = PDF::loadView('pdfs.items_list_site_pdf', compact('items'));
-       
-        $filename = 'Cipree' . '.pdf'; // Append '.pdf' to the filename
-
-        return $inventory->download($filename);
-
-
-
-        
-    }
+            ->where('inventory_items.quantity', '>', 0)
+            ->chunk(1000, function ($items) use ($pdf) {
+                $pdf->loadView('pdfs.items_list_site_pdf', ['items' => $items]);
+                // Append data or create separate pages for each chunk
+            });
+    
+        return $pdf->download('items_list.pdf');
+    
+}
 }
